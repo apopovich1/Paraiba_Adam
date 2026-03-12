4a25f89e415ab64136221153e95776b2bfeb8c41
apopovich1 <127229110+apopovich1@users.noreply.github.com>
2026-03-12 15:40:15 -0400

    Current changes to get cuisine and filters for frontend

--- backend/src/ParaibaModel.py
+++ backend/src/ParaibaModel.py
@@ -19,7 +19,7 @@ db = client["test"] # the database that houses the info is in test
 redditCollection = db["comments"] #this is where the unprocessed, scraped Reddit API data is
 paraibaCollection = db["paraibas"] #this collection is where the info from this file will go
 
-TEST = True
+TEST = False
 if TEST:
     # Test with hardcoded data
     comments = [
@@ -42,6 +42,25 @@ for loc in totLocations: #for every location in the list
         'name': loc['name'],
         '_id': loc['_id']
     } #add the location data from collection to that name 
+
+cuisineIndicators = { #These are words to match on for cuisines
+    "bbq", "barbecue", "barbeque", "korean", "thai", "chinese", "mexican",
+    "italian", "indian", "japanese", "sushi", "vietnamese", "mediterranean",
+    "greek", "french", "caribbean", "ethiopian", "cajun", "american",
+    "southern", "ramen", "poke", "tapas", "persian", "turkish", "lebanese",
+    "salvadoran", "cuban", "jamaican", "tex-mex", "seafood", "steakhouse",
+    "burger", "pizza", "wings", "sandwiches", "bagels", "deli", "bakery",
+    "brunch", "breakfast", "noodles", "dumpling", "dim sum"
+}
+
+def getCuisine(name, description, reviews):
+    #look within name, description, and reviews for cuisines
+    text = " ".join([ #join the name if found in the name, description, or reviews
+        name or "", description or "", " ".join(r.get('text', '') for r in reviews)
+    ]).lower()
+
+    cuisineFound = [cuisine for cuisine in cuisineIndicators if cuisine in text] 
+    return cuisineFound if cuisineFound else ["general"] #return general for cuisine if nothing matches
 
 def matchLocs(spaCy, existing): #this is a helper function to match spacy names to place API names 
     cleaned = spaCy.lower() #get spacy extracted name in lowercase
@@ -67,8 +86,8 @@ for i, comment in enumerate(comments, 1): #for every comment
     #above is to be used later to put into paraibas collection
     
     spaCyEntities = spaCy_full(commentText) #extract the name(s) with spacy
-    for i in spaCyEntities:
-        print(f"Locs found: {i}")
+    for loc in spaCyEntities: #had to change from i to loc: overwritten i a couple of times
+        print(f"Locs found: {loc}")
     
     if not spaCyEntities: #if not in the spaCyEntities list then continue
         continue
@@ -92,7 +111,12 @@ for i, comment in enumerate(comments, 1): #for every comment
             'text': commentText,
             'upvotes': upvotes,
             'link': link
-        })      
+        }) 
+
+    redditCollection.update_one( #issue with comments duplicating, so update the reddit comments here
+        {'_id': comment['_id']},
+        {'$set': {'processed': True}}
+    )     
 
 # Separate new vs existing locations
 newLocations = [name for cleanName, name in allLocations.items() if cleanName not in existingLocations] #if the name is in the total locations and not in paraibas collection then add to newLocations
@@ -140,23 +164,41 @@ if newLocations:  # if the name is new to the pariabas collection
                 averageUpvotes=avgUpVote, #get average up vote
                 sentimentScore=sentimentFull['compound'] if sentimentFull else 0 #get vader sentiment
             )
+
+            cuisineType = getCuisine(
+                gLoc['name'], gLoc.get('description'), gLoc.get('googlereviews', []) #run get cuisine to get the cuisine, if it does not find one it is general
+            )
  
-            paraibaCollection.insert_one({
-                'name': gLoc['name'], #name from places API
-                'address': gLoc['address'], #address from places API
-                'category': gLoc['category'], #category from places API
-                'reviewCount': gLoc['review_count'], #review count from places API
-                'rating': gLoc['rating'], #rating from places API
-                'description': gLoc.get('description'), #description from places API
-                'sentimentrating': sentimentFull['compound'] if sentimentFull else None, #This is a call from VADER and gets the compound number
-                'ranking': rankingResult['Paraíba Score'], #This is a call from ranking model
-                'upvotes': sum(comment['upvotes'] for comment in matching['comments']), #upvotes from comments collection
-                'mentionCount': len(matching['comments']), #This currently is not in the collection
-                'location': currentLocation, #this comes from description inputted into Places API, not in collection
-                'comments': [{'text': c['text']} for c in matching['comments']], #comments from comments collection, gets only the text from above
-                'googleReviews': gLoc.get('googlereviews', []), #reviews are comments from places API
-                'link': [comment['link'] for comment in matching['comments']], #link from comments collection and returns all associated links
-            }) 
+            paraibaCollection.update_one( #Changed from Insert to update to ensure duplicates are handled
+                {'name': gLoc['name']}, #name from places API, matches on the name
+                {'$setOnInsert': { #sets on insert if the document is being inserted for the first time
+                    'address': gLoc['address'], #address from places API
+                    'category': gLoc['category'], #category from places API
+                    'reviewCount': gLoc['review_count'], #review count from places API
+                    'rating': gLoc['rating'], #rating from places API
+                    'description': gLoc.get('description'), #description from places API
+                    'sentimentRating': sentimentFull['compound'] if sentimentFull else None, #This is a call from VADER and gets the compound number
+                    'ranking': rankingResult['Paraíba Score'], #This is a call from ranking model
+                    'upvotes': sum(comment['upvotes'] for comment in matching['comments']), #upvotes from comments collection
+                    'mentionCount': len(matching['comments']), #This currently is not in the collection
+                    'location': currentLocation, #this comes from description inputted into Places API, not in collection
+                    'comments': [{'text': c['text']} for c in matching['comments']], #comments from comments collection, gets only the text from above
+                    'googleReviews': gLoc.get('googlereviews', []), #reviews are comments from places API
+                    'link': [comment['link'] for comment in matching['comments']], #link from comments collection and returns all associated links
+                    'cuisineType': cuisineType, #get cuisine type
+                    'servesBreakfast': gLoc.get('serves_breakfast', False), #google has the ones below
+                    'servesBrunch': gLoc.get('serves_brunch', False), #All these do are return true or false
+                    'servesLunch': gLoc.get('serves_lunch', False),
+                    'servesDinner': gLoc.get('serves_dinner', False),
+                    'servesBeer': gLoc.get('serves_beer', False),
+                    'servesWine': gLoc.get('serves_wine', False),
+                    'servesVegetarian': gLoc.get('serves_vegetarian', False),
+                    'dineIn': gLoc.get('dine_in', False),
+                    'takeout': gLoc.get('takeout', False),
+                    'delivery': gLoc.get('delivery', False),
+                 }},
+                upsert=True #protect against duplicates
+            ) 
         else:
             print(f"No match found for {gLoc['name']}")
 
@@ -182,6 +224,7 @@ if updateTheLocs:
         getAllComments = [comment['text'] for comment in findOldComms.get('comments', [])] # get all existing comments at the mongo ID
 
         getAllComments.extend([comment['text'] for comment in newComments]) #add the new comments to the old ones
+        sentimentScore = analyzer(getAllComments) #get vader score of all associated comments
 
         rankingResult = paraibaScore( #same as above
             googleRating=exisitingLocInfo.get('rating', 0) if isinstance(exisitingLocInfo.get('rating'), (int, float)) else 0, #get rating
@@ -191,18 +234,15 @@ if updateTheLocs:
             sentimentScore=sentimentScore['compound'] if sentimentScore else 0 #get current sentiment score
         )
         
-        sentimentScore = analyzer(getAllComments) #get vader score of all associated comments
+        
         paraibaCollection.update_one(
             {'_id': exisitingLocInfo['_id']}, #for the id mentioned
             {
                 '$inc': {'upvotes': totUpvotes, 'mentionCount': len(newComments)}, #put in total upvotes and mention count which is the number of comments that mention it
                 '$push': {'comments': {'$each': newComments}}, #append the new comments
-                '$set': {'sentimentrating': sentimentScore['compound'], 'ranking': rankingResult['Paraíba Score'] } #set the new vader score in place of the old one as well as paraiba
+                '$set': {'sentimentRating': sentimentScore['compound'], 'ranking': rankingResult['Paraíba Score'] } #set the new vader score in place of the old one as well as paraiba
             }
         )
-        redditCollection.update_one( #this updates the comment to flag it to be true
-            {'_id': comment['_id']},
-            {'$set': {'processed': True}}
-        )
+        #got rid of updatin the processed flag as true here and moved it up
 
 client.close() #close mongo
